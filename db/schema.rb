1a006514709aca8f820ccb4b4be02c366019b918
Yonatan Amir <jonathanamir1@gmail.com>
2023-01-24 20:14:40 +0100

    models are validated

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_180633) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_184056) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
-    t.string "catagory"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
